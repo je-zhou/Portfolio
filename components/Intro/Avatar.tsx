ae7bac27df91118348cfa8cf80202eebcbe61f7e
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2023-07-11 22:02:45 +1000

    layout of avatar + made the project years not sticky

--- components/Intro/Avatar.tsx
+++ components/Intro/Avatar.tsx
@@ -3,16 +3,17 @@ import Image from 'next/image'
 
 export default function Avatar() {
 	return (
-		<div className='rounded-full bg-hoki flex items-center justify-center  overflow-hidden'>
-			<Image
-				className=''
-				src={"/avatar/Full-Hoodie.png"}
-				alt="Full Hoodie"
-				width={10000}
-				height={10000}
-			></Image>
-
-		</div >
+		<div className='w-[50vw] md:w-[25vw] h-[50vw] md:h-[25vw] rounded-full  bg-hoki flex justify-center items-end overflow-hidden  '>
+			<div className='w-full flex justify-center relative'>
+				<Image
+					className='h-full w-1/2'
+					src={"/avatar/Full-Hoodie.png"}
+					alt="Full Hoodie"
+					width={10000}
+					height={10000}
+				></Image>
+			</div>
+		</div>
 
 	)
 }
--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -4,18 +4,15 @@ import Avatar from './Avatar'
 
 export default function Intro() {
 	return (
-		<div className='flex flex-col w-full max-h-[90vh] h-full items-center bg-blue-600'>
-			<div className='flex w-full max-w-[2040px] h-screen flex-col justify-evenly md:flex-row md:items-center bg-green-400' >
-				<div className='flex flex-col space-y-8 items-center md:flex-1 md:items-start'>
+		<div className='flex flex-col w-full max-h-[85vh] h-full items-center'>
+			<div className='flex w-full max-w-[2040px] h-screen flex-col justify-evenly items-center md:flex-row md:items-center px-12' >
+				<div className='flex flex-col space-y-8 items-center md:flex-1 md:items-start '>
 					<p className='text-xl lg:text-4xl'>Hello! My Name is</p>
 					<h1 className='text-6xl lg:text-9xl font-bold'>Jerry Zhou</h1>
 				</div>
-				<div className='w-1/3 h-1/2 bg-yellow-500 flex justify-center items-center'>
-					<Avatar></Avatar>
-				</div>
-
+				<Avatar></Avatar>
 			</div>
 			<Scroll></Scroll>
-		</div>
+		</div >
 	)
 }
